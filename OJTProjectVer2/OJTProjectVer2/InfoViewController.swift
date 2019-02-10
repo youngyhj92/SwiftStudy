3defec8e2b2951d4d4bd5a3212c5d77fe598c758
youngyhj92 <makellan1926@gmail.com>
2019-02-11 00:17:58 +0900

    mainView + detail

---
+++ OJTProjectVer2/OJTProjectVer2/InfoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailInfoViewController.swift
+//  OJTProjectVer2
+//
+//  Created by hyunjun yang on 10/02/2019.
+//  Copyright © 2019 hj.yang. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController : UIViewController  {
+    
+}
